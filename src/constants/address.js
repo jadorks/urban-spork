181f05935d8f436644142a1781b50d61d2b392f6
jadorks <adorkorj@gmail.com>
2023-02-07 05:17:52 +0000

    Add contract ABI

---
+++ src/constants/address.js
@@ -0,0 +1,11 @@
+import { Mainnet, Localhost } from "@usedapp/core";
+
+export const TOKEN_ADDRESS = {
+  [Mainnet.chainId]: "0xF1B315293f358afc64Db8EddEf25C26D1592BA05",
+  [Localhost.chainId]: "0x1bc9E0F6FE0B4F38eFF1f19a92476240d76a0f1f",
+};
+
+export const STAKE_ADDRESS = {
+  [Mainnet.chainId]: "0x162a46c563D61ed8384dEA83D6908D838834cCDc",
+  [Localhost.chainId]: "0x6e9A38c23f8215bba5109166e090316096B47D0b",
+};
