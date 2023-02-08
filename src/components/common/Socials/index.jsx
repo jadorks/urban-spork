63462695d9bfe45462d99244697d950a0da2f171
jadorks <adorkorj@gmail.com>
2023-02-08 06:13:59 +0000

    Add social links

--- src/components/common/Socials/index.jsx
+++ src/components/common/Socials/index.jsx
@@ -7,10 +7,10 @@ import Medium from "@/assets/icons/medium.svg";
 const Socials = () => {
   return (
     <div className={styles.social_links}>
-      <a href="/#" rel="noopener noreferrer" target="_blank">
+      <a href="https://twitter.com/DwormDev" rel="noopener noreferrer" target="_blank">
         <img src={Twitter.src} alt="" />
       </a>
-      <a href="/#" rel="noopener noreferrer" target="_blank">
+      <a href="https://t.me/dwormportalgroup" rel="noopener noreferrer" target="_blank">
         <img src={Telegram.src} alt="" />
       </a>
       <a href="/#" rel="noopener noreferrer" target="_blank">
