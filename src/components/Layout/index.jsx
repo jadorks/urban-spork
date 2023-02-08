b554b28051b6fd42efc3dfc37ec9ee0ac7d9e9cb
jadorks <adorkorj@gmail.com>
2023-02-08 05:29:35 +0000

    Add metadata

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -6,7 +6,37 @@ import styles from "./layout.module.css";
 export default function Layout({ children }) {
   return (
     <>
-      <Head></Head>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="DWORM" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+
+        <title>DWORM | DAPP</title>
+      </Head>
       <div className={styles.Layout}>
         <NavBar />
         <main className={styles.content}>{children}</main>
--- src/pages/stake/index.jsx
+++ src/pages/stake/index.jsx
@@ -5,16 +5,22 @@ import styles from "./Stake.module.css";
 import StakeForm from "@/components/Stake/Forms/StakeForm";
 import UnstakeForm from "@/components/Stake/Forms/UnstakeForm";
 import ClaimForm from "@/components/Stake/Forms/Claim";
+import Head from "next/head";
 
 export default function Stake() {
   const [state, setState] = useState(0);
   return (
-    <div className="mb-8 sm:mb-16 w-full md:w-[40rem] flex flex-col gap-4 items-center">
-      <ContextSwitcher currentState={state} switchState={setState} />
-      {state == 0 && <StakeForm />}
-      {state == 1 && <UnstakeForm />}
-      {state == 2 && <ClaimForm />}
-      <ActivityWidget />
-    </div>
+    <>
+      <Head>
+        <title>DWORM | Stake</title>
+      </Head>
+      <div className="mb-8 sm:mb-16 w-full md:w-[40rem] flex flex-col gap-4 items-center">
+        <ContextSwitcher currentState={state} switchState={setState} />
+        {state == 0 && <StakeForm />}
+        {state == 1 && <UnstakeForm />}
+        {state == 2 && <ClaimForm />}
+        <ActivityWidget />
+      </div>
+    </>
   );
 }
